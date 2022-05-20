44b7e754397205f61e40c9a2875b7587bf5cf763
Paolo <paolo.a.campos@gmail.com>
2022-05-20 17:39:48 -0300

    intermediate commit

--- tests/Application/Entity/Customer.php
+++ tests/Application/Entity/Customer.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+* This file is part of the Odiseo Referrals Plugin package, a commercial software.
+* Only users who have purchased a valid license and accept to the terms of the License Agreement can install
+* and use this program.
+*
+* Copyright (c) 2018-2022 Odiseo - Pablo D'amico
+*/
+
 declare(strict_types=1);
 
-namespace Tests\Odiseo\SyliusMarketplacePlugin\Application\Entity;
-
+namespace Tests\Odiseo\SyliusReferralsPlugin\Application\Entity;
+          
 use Doctrine\ORM\Mapping as ORM;
 use Odiseo\SyliusReferralsPlugin\Entity\ReferralsProgramsTrait;
 use Sylius\Component\Core\Model\Customer as BaseCustomer;
